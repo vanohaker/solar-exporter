13399cb3027856a70dd2ba5a13938fca970399f5
iglazkov <iglazkov@mail.ru>
2024-07-12 14:47:06 +0300

    commit1

--- internal/app/start.go
+++ internal/app/start.go
@@ -1,143 +1,50 @@
 package app
 
 import (
-	"flag"
 	"fmt"
-	"log"
-	"net"
-	"net/http"
-	"os"
 	"runtime"
-	"runtime/debug"
-	"strconv"
-	"strings"
-	"time"
 
+	"github.com/gofiber/fiber/v2"
+	fiberlog "github.com/gofiber/fiber/v2/log"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/vanohaker/solar-exporter/internal/args"
-	"github.com/vanohaker/solar-exporter/pkg/smartwatt"
+	"github.com/vanohaker/solar-exporter/internal/options"
 )
 
-func getBuildInfo() (string, string, bool) {
-	var commitHash, commitTime string
-	var dirtyBuild bool
-	info, ok := debug.ReadBuildInfo()
-	if !ok {
-		return "", "", false
-	}
-	for _, kv := range info.Settings {
-		switch kv.Key {
-		case "vcs.revision":
-			commitHash = kv.Value
-		case "vcs.time":
-			commitTime = kv.Value
-		case "vcs.modified":
-			dirtyBuild = kv.Value == "true"
-		}
-	}
-	return commitHash, commitTime, dirtyBuild
-}
-
-func parseUnixSocketAddress(address string) (string, string, error) {
-	addressParts := strings.Split(address, ":")
-	addressPartsLength := len(addressParts)
-
-	if addressPartsLength > 3 || addressPartsLength < 1 {
-		return "", "", fmt.Errorf("address for unix domain socket has wrong format")
-	}
-
-	unixSocketPath := addressParts[1]
-	requestPath := ""
-	if addressPartsLength == 3 {
-		requestPath = addressParts[2]
-	}
-	return unixSocketPath, requestPath, nil
-}
-
-func getListener(listenAddress string) (net.Listener, error) {
-	var listener net.Listener
-	var err error
-
-	if strings.HasPrefix(listenAddress, "unix:") {
-		path, _, pathError := parseUnixSocketAddress(listenAddress)
-		if pathError != nil {
-			return listener, fmt.Errorf("parsing unix domain socket listen address %s failed: %w", listenAddress, pathError)
-		}
-		listener, err = net.ListenUnix("unix", &net.UnixAddr{Name: path, Net: "unix"})
-	} else {
-		listener, err = net.Listen("tcp", listenAddress)
-	}
-
-	if err != nil {
-		return listener, err
-	}
-	log.Printf("Listening on %s", listenAddress)
-	return listener, nil
-}
-
 func Run() {
-	flag.Parse()
-
-	commitHash, commitTime, dirtyBuild := getBuildInfo()
 	arch := fmt.Sprintf("%v/%v", runtime.GOOS, runtime.GOARCH)
-
-	log.Printf("SmartWatt ECO Prometheus Exporter version=%v commit=%v date=%v, dirty=%v, arch=%v, go=%v\n", args.Version, commitHash, commitTime, dirtyBuild, arch, runtime.Version())
-
-	if *args.DisplayVersion {
-		os.Exit(0)
+	config, err := options.LoadConfig()
+	if err != nil {
+		fiberlog.Fatal(err.Error())
 	}
-
-	log.Println("Starting prometheus metrics builder")
+	app := fiber.New(fiber.Config{
+		Immutable: true,
+	})
+	fiberlog.Infof("SmartWatt ECO Prometheus Exporter, arch=%v, go=%v\n", arch, runtime.Version())
 
 	registry := prometheus.NewRegistry()
 
 	buildInfoMetric := prometheus.NewGauge(
 		prometheus.GaugeOpts{
-			Name: "solarexporter_build_info",
-			Help: "Exporter build information",
+			Name: "smartwatteco_build_info",
+			Help: "SmartWatt ECO Exporter build information",
 			ConstLabels: prometheus.Labels{
-				"version": args.Version,
-				"commit":  commitHash,
-				"date":    commitTime,
-				"dirty":   strconv.FormatBool(dirtyBuild),
-				"arch":    arch,
-				"go":      runtime.Version(),
+				"arch": arch,
+				"go":   runtime.Version(),
 			},
 		},
 	)
 	buildInfoMetric.Set(1)
 	registry.MustRegister(buildInfoMetric)
 
-	log.Println("Starting serial port communications")
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("/")
+	})
+
+	app.Listen(fmt.Sprintf("%s:%v", config.Core.BindAddress, config.Core.BindPort))
 
 	// var voltage = new(float64)
 	// invertor.GetVoltages(serialsession, voltage)
 
-	go smartwatt.StartColllect(registry)
+	// go smartwatt.StartColllect(registry)
 
-	srv := http.Server{
-		ReadHeaderTimeout: 5 * time.Second,
-	}
-
-	http.Handle(*args.MetricsPath, promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintf(w, `<!DOCTYPE html>
-			<title>SmartWatt ECO Exporter</title>
-			<h1>SmartWatt ECO Exporter</h1>
-			<p><a href=%q>Metrics</a></p>`,
-			*args.MetricsPath)
-		if err != nil {
-			log.Printf("Error while sending a response for the '/' path: %v", err)
-		}
-	})
-
-	listener, err := getListener(*args.ListenAddr)
-	if err != nil {
-		log.Fatalf("Could not create listener: %v", err)
-	}
-
-	log.Println("SmartWatt ECO exporter started")
-	log.Fatal(srv.Serve(listener))
 }
--- internal/args/args.go
+++ internal/args/args.go
@@ -1,34 +1,28 @@
 package args
 
-import (
-	"flag"
-	"os"
-	"strconv"
-)
-
-func getEnv(key, defaultValue string) string {
-	value, ok := os.LookupEnv(key)
-	if !ok {
-		return defaultValue
-	}
-	return value
-}
+// func getEnv(key, defaultValue string) string {
+// 	value, ok := os.LookupEnv(key)
+// 	if !ok {
+// 		return defaultValue
+// 	}
+// 	return value
+// }
 
 var (
-	Version string
+// Version string
 
-	// default vars values
-	defaultSerialPortName        = getEnv("SERIAL_PORT", "/dev/ttyUSB0")
-	defaultSerialPortBaudRate, _ = strconv.Atoi(getEnv("SERIAL_PORT_BAUD_RATE", "2400"))
-	defaultMetricsPath           = getEnv("METRICS_PATH", "/metrics")
-	defaultListenAddr            = getEnv("LISTEN_ADDRESS", "0.0.0.0:9678")
-	defaultScrapeInterval, _     = strconv.Atoi(getEnv("SCRAPE_INTERVAL", "5"))
+// default vars values
+// defaultSerialPortName        = getEnv("SERIAL_PORT", "/dev/ttyUSB0")
+// defaultSerialPortBaudRate, _ = strconv.Atoi(getEnv("SERIAL_PORT_BAUD_RATE", "2400"))
+// defaultMetricsPath           = getEnv("METRICS_PATH", "/metrics")
+// defaultListenAddr            = getEnv("LISTEN_ADDRESS", "0.0.0.0:9678")
+// defaultScrapeInterval, _     = strconv.Atoi(getEnv("SCRAPE_INTERVAL", "5"))
 
-	SerialPortName     = flag.String("serialPortName", defaultSerialPortName, "Serial port name of the connected inverter")
-	SerialPortBaudRate = flag.Int("serialPortBaudRate", defaultSerialPortBaudRate, "Serial port speed")
-	DisplayVersion     = flag.Bool("version", false, "Display SmartWatt ECO exporter version")
-	DebugMode          = flag.Bool("debug", false, "Debug mode")
-	MetricsPath        = flag.String("metricsPath", defaultMetricsPath, "Url with metrics data")
-	ListenAddr         = flag.String("webListenAddr", defaultListenAddr, "TCP port where exporter started")
-	ScrapeInterval     = flag.Int("scrapeInterval", defaultScrapeInterval, "Scrape interval in secconds")
+// SerialPortName     = flag.String("serialPortName", defaultSerialPortName, "Serial port name of the connected inverter")
+// SerialPortBaudRate = flag.Int("serialPortBaudRate", defaultSerialPortBaudRate, "Serial port speed")
+// DisplayVersion = flag.Bool("version", false, "Display SmartWatt ECO exporter version")
+// DebugMode      = flag.Bool("debug", false, "Debug mode")
+// MetricsPath        = flag.String("metricsPath", defaultMetricsPath, "Url with metrics data")
+// ListenAddr         = flag.String("webListenAddr", defaultListenAddr, "TCP port where exporter started")
+// ScrapeInterval     = flag.Int("scrapeInterval", defaultScrapeInterval, "Scrape interval in secconds")
 )
